411c7a1477ed16dc4890e208bb785f395cbf4a53
Connor Winiarczyk <connorwiniarczyk@gmail.com>
2020-09-04 13:46:06 -0400

    Moved from express / javascript to rocket /rust

---
+++ src/content_parser.rs
@@ -0,0 +1,36 @@
+use std::fs;
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize)]
+pub struct GalleryImage {
+	pub id: String,
+	pub title: String,
+	pub image: String,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Content {
+	pub gallery: Vec<GalleryImage>,
+}
+
+impl Default for Content {
+	fn default () -> Content {
+		Content {
+			gallery: Vec::new(),
+		}
+	}
+}
+
+pub fn get_content () -> Content {
+	let file = fs::read_to_string("content.toml").expect("error"); 
+	let info: std::result::Result<Content, toml::de::Error> = toml::from_str(&file);
+	let result = match info {
+		Ok(value) => value,
+		Err(e) => {
+			println!("Error parsing content.toml: {}", e);
+			Content::default()
+		}
+	};
+
+	return result
+}
---
+++ src/main.rs
@@ -0,0 +1,26 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+extern crate tera;
+
+mod content_parser;
+
+use rocket_contrib::serve::StaticFiles;
+use rocket_contrib::templates::Template;
+
+#[get("/")]
+fn index() -> Template {
+    let context = content_parser::get_content();
+	Template::render("index", &context)
+}
+
+fn main() {
+	rocket::ignite()
+		.attach(Template::fairing())
+		.mount("/", routes![index])
+		.mount("/stylesheets", StaticFiles::from("./public/stylesheets"))
+		.mount("/scripts", StaticFiles::from("./public/scripts"))
+		.mount("/fonts", StaticFiles::from("./public/fonts"))
+		.mount("/media", StaticFiles::from("../../Shared/portfolio_media"))
+		.launch();
+}
